024855b2e0a5cf719252b537c1eac30b17b58f7e
kerry jalal <khariy34@hotmail.com>
2023-01-31 18:54:57 +0100

    add Details entity + add one to one in AppUser

---
+++ src/main/java/lexicon/workshop15springdata/entity/Details.java
@@ -0,0 +1,33 @@
+package lexicon.workshop15springdata.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Details {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String name;
+
+
+    private LocalDate birthDate;
+
+    public Details(String email, String name, LocalDate birthDate) {
+        this.email = email;
+        this.name = name;
+        this.birthDate = birthDate;
+    }
+}
