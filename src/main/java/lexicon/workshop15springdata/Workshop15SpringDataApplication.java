2cad23acb94a961da8f7286d06f160e97329f19e
kerry jalal <khariy34@hotmail.com>
2023-01-31 12:41:15 +0100

    Initial commit

---
+++ src/main/java/lexicon/workshop15springdata/Workshop15SpringDataApplication.java
@@ -0,0 +1,13 @@
+package lexicon.workshop15springdata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Workshop15SpringDataApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Workshop15SpringDataApplication.class, args);
+    }
+
+}
---
+++ src/test/java/lexicon/workshop15springdata/Workshop15SpringDataApplicationTests.java
@@ -0,0 +1,13 @@
+package lexicon.workshop15springdata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Workshop15SpringDataApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
